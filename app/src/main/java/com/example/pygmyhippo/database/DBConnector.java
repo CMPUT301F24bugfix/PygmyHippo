f207887f72581678d548c88ee5ee67143ed351d8
SuperCreativeUniqueUsername <gfrankiw@ualberta.ca>
2024-10-20 11:34:48 -0600

    added some comments explaining what each  function does

--- app/src/main/java/com/example/pygmyhippo/database/DBConnector.java
+++ app/src/main/java/com/example/pygmyhippo/database/DBConnector.java
@@ -2,9 +2,25 @@ package com.example.pygmyhippo.database;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
+// DBCOnnector Class
+// Initializes the connection to the database
 public class DBConnector {
     private FirebaseFirestore db;
 
+
+    /*
+   Function initializes the current instance of the DB
+   */
+    public void DBConnect(){
+        this.db = FirebaseFirestore.getInstance();
+    }
+
+    /*
+    Function returns the current instance of the FirebaseFirestore database
+    returns:
+        FirebaseFirestore: current instance of the FirebaseFirestore
+    */
     public FirebaseFirestore getDB() {
         return this.db;
     }
