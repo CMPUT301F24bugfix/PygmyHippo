2941b3a9c4caa814385176aee40170b766a0008a
SuperCreativeUniqueUsername <gfrankiw@ualberta.ca>
2024-10-20 21:50:59 -0600

    added gradle to git since it has database specific entries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.4.0")) // for database
+    implementation("com.google.firebase:firebase-firestore") // for database
     implementation("com.google.android.material:material:1.6.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.appcompat)
