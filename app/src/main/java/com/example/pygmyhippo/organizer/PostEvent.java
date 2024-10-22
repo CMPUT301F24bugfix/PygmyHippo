979fc7d14ac4f297d61118f919b30e1389cc6a69
SuperCreativeUniqueUsername <gfrankiw@ualberta.ca>
2024-10-22 12:21:00 -0600

    rough draft for posting event

--- app/src/main/java/com/example/pygmyhippo/organizer/PostEvent.java
+++ app/src/main/java/com/example/pygmyhippo/organizer/PostEvent.java
@@ -1,9 +1,11 @@
 package com.example.pygmyhippo.organizer;
 
+import android.graphics.Insets;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,8 +14,8 @@ import com.example.pygmyhippo.databinding.OrganiserPostEventBinding;
 
 /**
  * This fragment will hold the post event
- * @author none
- * @version none
+ * @author griffin
+ * @version 1.0
  * No returns and no parameters
  */
 public class PostEvent extends Fragment {
@@ -23,14 +25,13 @@ public class PostEvent extends Fragment {
 
     /**
      * Creates the view
-     * @author none
+     * @author griffin
      * @param inflater not sure
      * @param container not sure
      * @param savedInstanceState  not surre
      * @return root not sure
      */
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = OrganiserPostEventBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
