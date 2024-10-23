3ab73e8fb2ccc17a417f5037903d83fce3ae5174
Katharine Zhong <kathari2@ualberta.ca>
2024-10-22 20:26:48 -0600

    added some functionality

---
+++ app/src/main/java/com/example/pygmyhippo/user/EventFragment.java
@@ -0,0 +1,60 @@
+package com.example.pygmyhippo.user;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.pygmyhippo.R;
+import com.example.pygmyhippo.common.Event;
+
+/**
+ * This fragment will hold a single events
+ * @author Katharine
+ * @version 1.0
+ * No returns and no parameters
+ */
+public class EventFragment extends Fragment {
+
+    // populate single event page with event information
+    private Event event;
+
+    public EventFragment() {
+    }
+
+    // this might be useful
+    public EventFragment(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.user_fragment_myevent, container, false);
+
+        Button registerButton = view.findViewById(R.id.registerButton);
+
+        // with a check, check to see if event has been registered for, if so, register (set text
+        // to checkmark, add to lists and stuff, if event has not been register for, set text back
+        // to register and remove from lists and stuff
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // set text of register button to checkmark and disable the button
+                registerButton.setText("✔");
+                registerButton.setEnabled(false);
+            }
+        });
+        return view;
+    }
+}
