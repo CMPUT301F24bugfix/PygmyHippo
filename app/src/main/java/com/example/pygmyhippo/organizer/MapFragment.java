edff1ceb4a932438cc4580d7bc7d2e7c667b16ae
“Kori <kkozicki@ualberta.ca>
2024-11-18 20:34:30 -0700

    Starting map fragment

---
+++ app/src/main/java/com/example/pygmyhippo/organizer/MapFragment.java
@@ -0,0 +1,6 @@
+package com.example.pygmyhippo.organizer;
+
+public class MapFragment {
+    // https://developers.google.com/maps/documentation/android-sdk/reference/com/google/android/libraries/maps/SupportMapFragment
+    // https://github.com/jonahb/equirectangular_projection/blob/master/equirectangularProjection.js
+}
